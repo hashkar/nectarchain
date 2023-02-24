8a517aeb43c41878fdd183af71552b8f3e94467a
Halim Ashkar <halim.ashkar@cea.fr>
2023-02-24 13:57:51 +0100

    fixed a bug in charge intergration (high and low gain were the same). Also added in the processor the handling of the case where not all the processors are chosen (in wrtiting the results fits file)

--- src/nectarchain/dqm/charge_integration.py
+++ src/nectarchain/dqm/charge_integration.py
@@ -36,7 +36,7 @@ class ChargeIntegration_HighLowGain(dqm_summary):
 
     def ProcessEvent(self, evt):
         #print("test", evt.r0.tel[0].waveform[0])
-        waveform=evt.r0.tel[0].waveform[0]
+        waveform=evt.r0.tel[0].waveform[self.k]
         image, peakpos = self.integrator(waveform,0,np.zeros(self.Chan, dtype = int))
 
         if evt.trigger.event_type.value == 32: #count peds 
